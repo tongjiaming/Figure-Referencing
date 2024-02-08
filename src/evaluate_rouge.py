83b75ba9c7aa50839f9945b15932ad1b5c2686b9
tongjiaming <jiaming.tong@uzh.ch>
2024-02-08 19:52:34 +0100

    Initial commit

--- src/evaluate_rouge.py
+++ src/evaluate_rouge.py
@@ -1,44 +1,23 @@
 import evaluate
-import json
+from load_data import data_loader
+import time
 
 
+start_time = time.time()
 DATA_PATH = '../output/PMCOA_out2.json'
 rouge = evaluate.load('rouge')
-
 
 total = 0
 correct = [0, 0, 0, 0]
 
-with open(DATA_PATH) as file:
-    for line in file:
-        data = json.loads(line)
+loader = data_loader(DATA_PATH)
 
-        if len(data['referenced_items']) <= 1:
-            continue
-
-        for reference in data['references']:
+while True:
+    try:
+        queries, targets, candidates, candidate_labels = next(loader)
+        for query, target in zip(queries, targets):
             total = total + 1
             print('Working on sample {}'.format(total))
-
-            sentence_id = reference['sentence']
-            start = reference['start']
-            end = reference['end']
-            candidates = []
-            candidate_labels = []
-
-            for sentence in data['sentences']:
-                if sentence_id == sentence['id']:
-                    for paragraph in data['paragraphs']:
-                        if paragraph['id'] == sentence['paragraph']:
-                            sentence_text = paragraph['text'][sentence['start']:sentence['end']]
-                            break
-                    break
-
-            query = sentence_text[:start] + '<ref>' + sentence_text[:end]
-            gt = reference['target']
-            for candidate in data['referenced_items']:
-                candidates.append(candidate['caption'])
-                candidate_labels.append(candidate['id'])
 
             queries = [query] * len(candidates)
 
@@ -56,13 +35,17 @@ with open(DATA_PATH) as file:
             prediction_rougeL = candidate_labels[rougeL.index(max(rougeL))]
             prediction_rougeLsum = candidate_labels[rougeLsum.index(max(rougeLsum))]
 
-            correct[0] = correct[0] + (gt == prediction_rouge1)
-            correct[1] = correct[1] + (gt == prediction_rouge2)
-            correct[2] = correct[2] + (gt == prediction_rougeL)
-            correct[3] = correct[3] + (gt == prediction_rougeLsum)
+            correct[0] = correct[0] + (target == prediction_rouge1)
+            correct[1] = correct[1] + (target == prediction_rouge2)
+            correct[2] = correct[2] + (target == prediction_rougeL)
+            correct[3] = correct[3] + (target == prediction_rougeLsum)
+
+    except StopIteration:
+        break
 
 print('Total number of samples: {}'. format(total))
 print('Precision Using rouge1: {}'.format(correct[0] / total))
 print('Precision Using rouge2: {}'.format(correct[1] / total))
 print('Precision Using rougeL: {}'.format(correct[2] / total))
 print('Precision Using rougeLsum: {}'.format(correct[3] / total))
+print("Finished in {} seconds".format(time.time() - start_time))
---
+++ src/load_data.py
@@ -0,0 +1,62 @@
+import json
+
+DATA_PATH = '../output/PMCOA_out2.json'
+
+
+def data_loader(data_path):
+    with open(data_path) as file:
+        for line in file:
+
+            data = json.loads(line)
+
+            queries = []
+            targets = []
+            candidates = []
+            candidate_labels = []
+
+            if len(data['referenced_items']) <= 1:
+                continue
+
+            for reference in data['references']:
+                sentence_id = reference['sentence']
+                start = reference['start']
+                end = reference['end']
+
+                for sentence in data['sentences']:
+                    if sentence_id == sentence['id']:
+                        for paragraph in data['paragraphs']:
+                            if paragraph['id'] == sentence['paragraph']:
+                                sentence_text = paragraph['text'][sentence['start']:sentence['end']]
+                                break
+                        break
+
+                query = sentence_text[:start] + '<ref>' + sentence_text[:end]
+                queries.append(query)
+
+                target = reference['target']
+                targets.append(target)
+
+            for candidate in data['referenced_items']:
+                candidates.append(candidate['caption'])
+                candidate_labels.append(candidate['id'])
+
+            assert len(queries) == len(targets) and len(queries) != 0
+            assert len(candidates) == len(candidate_labels) and len(candidates) != 0
+
+            yield queries, targets, candidates, candidate_labels
+
+
+def main():
+    total = 0
+    loader = data_loader(DATA_PATH)
+    while True:
+        try:
+            queries, targets, candidates, candidate_labels = next(loader)
+            total = total + 1
+            print(total)
+        except StopIteration:
+            break
+
+
+if __name__ == "__main__":
+    main()
